f19be5a5f83480059028f9e1be286af84fc17815
lingtregetter <lingtregetter466@gmail.com>
2023-05-02 22:22:33 +0300

    PetListPage: dynamic table rows

---
+++ www/src/app/interfaces/pet.interface.ts
@@ -0,0 +1,7 @@
+export interface Pet {
+  readonly name: string;
+  readonly code: number;
+  readonly type: any; // TODO: replace with enum value
+  readonly furColor: any; // TODO: replace with enum value
+  readonly country: any; // TODO: replace with enum value
+}
--- www/src/app/pages/pets/pet-list-page/pet-list-page.component.ts
+++ www/src/app/pages/pets/pet-list-page/pet-list-page.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Pet } from 'src/app/interfaces/pet.interface';
 
 @Component({
   selector: 'app-pet-list-page',
   templateUrl: './pet-list-page.component.html',
-  styleUrls: ['./pet-list-page.component.scss']
+  styleUrls: ['./pet-list-page.component.scss'],
 })
 export class PetListPageComponent {
-
+  petList: Pet[] = [
+    {
+      name: 'Mustikas',
+      code: 123243424234,
+      type: 'Cat',
+      furColor: 'Black',
+      country: 'Estonia',
+    },
+    {
+      name: 'Roy',
+      code: 123243424234,
+      type: 'Dog',
+      furColor: 'White',
+      country: 'Norway',
+    },
+    {
+      name: 'Charles',
+      code: 123243424234,
+      type: 'Horse',
+      furColor: 'Brown',
+      country: 'Latvia',
+    },
+  ];
 }
