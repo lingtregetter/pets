7db39b6f279bf7baaa5c9bfbaca4ccd5a2d00763
lingtregetter <lingtregetter466@gmail.com>
2023-05-02 19:46:38 +0300

    add page-container component

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PageContainerComponent } from './components/page-container/page-container.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent],
+  declarations: [AppComponent, FooterComponent, PageContainerComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
