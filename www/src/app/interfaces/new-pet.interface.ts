7f32e18159e945b54b0ad5426031bb06d8739f84
lingtregetter <lingtregetter466@gmail.com>
2023-05-04 16:28:02 +0300

    Pet and new pet update

--- www/src/app/interfaces/new-pet.interface.ts
+++ www/src/app/interfaces/new-pet.interface.ts
@@ -1,7 +1,7 @@
 export interface NewPet {
   readonly userId: number;
   readonly name: string;
-  readonly code: number;
+  readonly code: string;
   readonly typeId: number;
   readonly furColorId: number;
   readonly countryOfOriginId: number;
--- www/src/app/interfaces/pet.interface.ts
+++ www/src/app/interfaces/pet.interface.ts
@@ -1,7 +1,7 @@
 export interface Pet {
   readonly id: number;
   readonly name: string;
-  readonly code: number;
+  readonly code: string;
   readonly type: any; // TODO: replace with enum value
   readonly furColor: any; // TODO: replace with enum value
   readonly countryOfOrigin: any; // TODO: replace with enum value
