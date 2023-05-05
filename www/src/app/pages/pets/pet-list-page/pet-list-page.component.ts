9467e471aed45c278142c4aab3058e96961477af
lingtregetter <lingtregetter466@gmail.com>
2023-05-05 20:45:52 +0300

    Pet sorting

--- www/src/app/pages/pets/pet-list-page/pet-list-page.component.ts
+++ www/src/app/pages/pets/pet-list-page/pet-list-page.component.ts
@@ -9,6 +9,7 @@ import { PetService } from 'src/app/services/pet.service';
 })
 export class PetListPageComponent implements OnInit {
   petList: Pet[] = [];
+  sorted: boolean = false;
 
   constructor(private readonly petService: PetService) {}
 
@@ -18,5 +19,49 @@ export class PetListPageComponent implements OnInit {
 
   async init(): Promise<void> {
     this.petList = await this.petService.getPets();
+  }
+
+  sortByName() {
+    this.sorted
+      ? this.petList.sort((a, b) => a.name.localeCompare(b.name))
+      : this.petList.sort((a, b) => b.name.localeCompare(a.name));
+
+    this.sorted = !this.sorted;
+  }
+
+  sortByCode() {
+    this.sorted
+      ? this.petList.sort((a, b) => a.code.localeCompare(b.code))
+      : this.petList.sort((a, b) => b.code.localeCompare(a.code));
+
+    this.sorted = !this.sorted;
+  }
+
+  sortByType() {
+    this.sorted
+      ? this.petList.sort((a, b) => a.type.localeCompare(b.type))
+      : this.petList.sort((a, b) => b.type.localeCompare(a.type));
+
+    this.sorted = !this.sorted;
+  }
+
+  sortByColor() {
+    this.sorted
+      ? this.petList.sort((a, b) => a.furColor.localeCompare(b.furColor))
+      : this.petList.sort((a, b) => b.furColor.localeCompare(a.furColor));
+
+    this.sorted = !this.sorted;
+  }
+
+  sortByCountry() {
+    this.sorted
+      ? this.petList.sort((a, b) =>
+          a.countryOfOrigin.localeCompare(b.countryOfOrigin)
+        )
+      : this.petList.sort((a, b) =>
+          b.countryOfOrigin.localeCompare(a.countryOfOrigin)
+        );
+
+    this.sorted = !this.sorted;
   }
 }
